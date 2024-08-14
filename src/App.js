3221e671d771f14b56ac993087d0dbfd33322e55
Willian Melo <willian.vieirademelo@gmail.com>
2024-08-14 20:36:58 -0300

    Feat: implementando componente InputForm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React from 'react';
+import InputForm from './components/InputForm';
+
 function App() {
+
+  const fetchSuppliers = (consumption) => {
+    const suppliers = [      
+      {
+        "name": "Fornecedor X",
+        "logo": "url_to_logo",
+        "state": "Estado",
+        "cost_per_kwh": 0.12,
+        "min_kwh_limit": 10000,
+        "total_clients": 200,
+        "average_rating": 4.5
+      },
+      {
+        "name": "Fornecedor Y",
+        "logo": "url_to_logo",
+        "state": "Estado2",
+        "cost_per_kwh": 0.11,
+        "min_kwh_limit": 15000,
+        "total_clients": 100,
+        "average_rating": 3.5
+      }
+    ]
+
+    const filteredSuppliers = suppliers.filter((supplier)=>consumption > supplier.min_kwh_limit)
+
+    console.log(filteredSuppliers) 
+    
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Escolha o Melhor Fornecedor de Energia</h1>
+      <InputForm onSubmit={fetchSuppliers} />
+      
     </div>
   );
 }
