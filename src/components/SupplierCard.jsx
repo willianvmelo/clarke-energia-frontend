13ea7d584d94085969345d266d3960ff50d3319a
Willian Melo <willian.vieirademelo@gmail.com>
2024-08-17 11:57:13 -0300

    Style: estilizando aplicativo.

--- src/components/SupplierCard.jsx
+++ src/components/SupplierCard.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 const SupplierCard = ({ supplier }) => {
     return (
-        <div className="supplier-card">
-            <img src={supplier.logo} alt={`${supplier.name} logo`} />
-            <h2>{supplier.name}</h2>
-            <p>Estado: {supplier.state}</p>
-            <p>Custo por kWh: R${supplier.cost_per_kwh}</p>
-            <p>Limite Mínimo de kWh: {supplier.min_kwh_limit}</p>
-            <p>Total de Clientes: {supplier.total_clients}</p>
-            <p>Avaliação Média: {supplier.average_rating}</p>
+        
+        <div className="card me-3 mb-3">
+            <img className="card-img-top" src={supplier.logo} alt={`${supplier.name} logo`} />
+            <div className="card-body" >
+                <h5 className="card-title">{supplier.name}</h5>
+                <p className="card-text"><b>Estado:</b> {supplier.state}</p>
+                <p className="card-text"><b>Custo por kWh:</b> R${supplier.cost_per_kwh}</p>
+                <p className="card-text"><b>Limite Mínimo de kWh:</b> {supplier.min_kwh_limit}</p>
+                <p className="card-text"><b>Total de Clientes:</b> {supplier.total_clients}</p>
+                <p className="card-text"><b>Avaliação Média:</b> {parseFloat(supplier.average_rating).toFixed(1)}</p>
+            </div>
+            
         </div>
+        
     );
 };
 
--- src/components/SupplierList.jsx
+++ src/components/SupplierList.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import SupplierCard from './SupplierCard';
+import { Spinner } from 'react-bootstrap';
 
-const SupplierList = ({ suppliers }) => {
-    console.log(suppliers)
-    if (suppliers.length === 0) {
-        return <p>Nenhum fornecedor encontrado para o consumo informado.</p>;
+const SupplierList = ({ suppliers, loading }) => {
+    
+    if (loading) {
+        return (
+            <div className="d-flex justify-content-center">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Buscando Fornecedores...</span>
+                </Spinner>
+            </div>
+        );
+    }
+
+    if (!suppliers.length) {
+        return (
+            <div className="alert alert-warning text-center" role="alert">
+                Nenhum fornecedor encontrado.
+            </div>
+        );
     }
 
     return (
-        <div className="supplier-list">
+        <div className="row justify-content-start align-items-start ms-1">
             {suppliers.map((supplier, index) => (
                 <SupplierCard key={index} supplier={supplier} />
             ))}
